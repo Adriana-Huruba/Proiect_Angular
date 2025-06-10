5e3e09ac818b9f432c5e22d9da9e10e18640a3ab
Isabela0524 <elena.ciutacu@student.unitbv.ro>
2025-06-10 21:59:42 +0300

    implemented the first part of the login

--- src/app/features/login-page/login-page.component.ts
+++ src/app/features/login-page/login-page.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=]).+$')
+      ]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password, rememberMe } = this.loginForm.value;
+
+      //simulare login pe care o inlocuim cu service mai tarziu
+      console.log('Logging in with:', email, password, rememberMe);
+
+      if (rememberMe) {
+        localStorage.setItem('userEmail', email);
+      }
+
+      this.router.navigate(['/movies']); //redirectioneaza dupa login
+    }
+  }
 
 }
